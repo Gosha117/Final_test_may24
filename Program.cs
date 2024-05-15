1aa6bf44c44323f7ab44b23e1fc120cc260cf346
Gosha117 <kometa210@gmail.com>
2024-05-16 01:12:20 +0300

    добавление процедуры вывода исходного массива

--- Program.cs
+++ Program.cs
@@ -3,11 +3,27 @@
 // Первоначальный массив можно ввести с клавиатуры,либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+string[] str = { "ruw", "oeru", "qo", "rur4o", "2", "tu", "t", "ry", "386g", "etyerye", "57", "dh", "g", "h", "re", "24", "7f", "785", "6cvb", "ete", "sge", "r", "ed", "w" };
+// Создание массива
+// void Create_array()
+// {
+//     string[] str = { "ruw", "oeru", "qo", "rur4o", "2", "tu", "t", "ry", "386g", "etyerye", "57", "dh", "g", "h", "re", "24", "7f", "785", "6cvb", "ete", "sge", "r", "ed", "w" };
+// }
+
+// Вывод массива
+void Print_array()
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write(str[i] + " ");
+    }
+}
+
+//Create_array();
+Print_array();
 
 
 
-
-
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 
